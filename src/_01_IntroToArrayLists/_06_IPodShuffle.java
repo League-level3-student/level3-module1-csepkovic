a908b75cf1ef659c8ec2dc00ae08e3bb8c37f7fd
League <league@WTS9.attlocal.net>
2018-12-02 14:29:20 -0800

    Week 1 (Final Month)

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -3,14 +3,16 @@ package _01_IntroToArrayLists;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 //Copyright The League of Amazing Programmers, 2015
 
 public class _06_IPodShuffle{
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				
-				
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -18,7 +20,48 @@ public class _06_IPodShuffle{
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
 		 */
-		
+				JFrame frame = new JFrame();
+				JPanel panel = new JPanel();
+				frame.add(panel);
+				frame.setSize(1000, 1000);
+				frame.setVisible(true);
+				ArrayList<Song> songs = new ArrayList<Song>();
+				
+				Song boogieNights = new Song("boogieNights.mp3");
+				songs.add(boogieNights);
+				JButton button1 = new JButton();
+				button1.setText("Boogie Nights");
+//				button1.addActionListener(this);
+				panel.add(button1);
+				
+				Song boogieWonderland = new Song("boogieWonderland.mp3");
+				songs.add(boogieWonderland);
+				JButton button2 = new JButton();
+				button2.setText("Boogie Wonderland");
+				panel.add(button2);
+				
+				Song gotMeLovinYou = new Song("gotMeLovinYou.mp3");
+				songs.add(gotMeLovinYou);
+				JButton button3 = new JButton();
+				button1.setText("Got Me Loving You");
+				panel.add(button3);
+				
+				Song whoHasTheTime = new Song("whoHasTheTime.mp3");
+				songs.add(whoHasTheTime);
+				JButton button4 = new JButton();
+				button1.setText("Who Has The Time");
+				panel.add(button4);
+				
+				Song whatYouWontDo = new Song("whatYouWontDoForLove.mp3");
+				songs.add(whatYouWontDo);
+				JButton button5 = new JButton();
+				button1.setText("What You Won't Do For Love");
+				panel.add(button5);
+				
+				for (int i = 0; i < songs.size(); i++) {
+					
+					
+				}
 	}
 	
 	public static void main(String[] args) {
